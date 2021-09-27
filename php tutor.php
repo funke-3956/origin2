c42c994cd08968f92bb8acc7e91d4209adb930a1
Kostiantyn Chobotar <koatyachobotar@gmail.com>
2021-09-27 13:33:37 +0200

    Add script for render a multiply table

--- php tutor.php
+++ php tutor.php
@@ -6,16 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        td{
+          border: 1px solid black;
+
+        }
+    </style>
 </head>
 <body>
 
 </body>
 </html>
 <?php
+echo "<table>\n";
+$i = 1;
+while($i<=11){
 
-const UNCHANGE = "You can't change me";
-
-
-echo UNCHANGE;
-
+        echo "\t<tr>\n";
+    $colls = 1;
+    while($colls<=3){
+        echo "\t \t <td>Row - $i /n, Coll - $colls</td> \n";
+        $colls++;
+    }
+    $i++;
+    echo "\t</tr>\n";
+}
+echo "</table>"
 ?>
